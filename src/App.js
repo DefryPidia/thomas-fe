8f5ea5e0fe3fd30e5a780a036e948ac44513519d
DefryPidia <realdefrifidia@gmail.com>
2023-02-22 22:32:36 +0700

    Upgrade folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("Thomas");
+
+  function getToBe() {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then((response) => {
+        setText(response.name);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={getToBe}>Click me</button>
+          <p>{text}</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
